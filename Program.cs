f8a33ccc5331b0dabd24dd8e820c0ca9c3743dbd
nikitailinskii <anastasiabalickaa435@gmail.com>
2023-02-13 11:17:27 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задача 1
+
+            //Console.Write("n=");
+            //int n = int.Parse(Console.ReadLine());
+            //int[] array = new int[n];
+            //for (int i = 0; i < array.Length; i++)
+            //{
+            //    Console.Write($"array[{i}]=");
+            //    array[i] = int.Parse(Console.ReadLine());
+            //}
+            //int k = array[0];
+            //for (int i = 0; i < array.Length; i++)
+            //{
+            //    array[i] += k;
+            //}
+            //for (int i = 0; i < array.Length; i++)
+            //{
+            //    Console.WriteLine($"array[{i}]={array[i]}");
+            //}
+
+            //Задача 2
+
+            //int[] array = new int[6] { 4, 8, -2, 5, 6, 9 };
+            //int count = 0;
+            //foreach (int elem in array) 
+            //{
+            //    if (elem % 2 == 0) count++;
+            //}
+            //Console.WriteLine($"count={count}");
+
+            //Задача 3
+            //Console.Write("n=");
+            //int n = int.Parse(Console.ReadLine());
+            //int[] array = new int[n];
+            //Random rnd = new Random();
+            //for (int i = 0; i < array.Length; i++)
+            //{
+            //    array[i] = rnd.Next(-10,10);
+            //    Console.WriteLine($"array[{i}]={array[i]}");
+            //}
+            //int max = array[0];
+            //for (int i = 0; i < array.Length; i++)
+            //{
+            //    if( array[i] > max)
+            //    {
+            //        max = array[i];
+            //    }
+            //}
+            //Console.WriteLine($"max={max}");
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
